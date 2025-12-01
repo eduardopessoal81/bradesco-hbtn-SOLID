c6c467062df290c9aab38fe9a278b9d2e61604f8
eduardodesenvolvedor <eduardofreire.contato@gmail.com>
2025-12-01 18:22:48 -0300

    Comments

---
+++ 0x04/cleanCode/Comments.java
@@ -0,0 +1,33 @@
+import java.util.Date;
+import java.util.List;
+
+public class Comments {
+
+    public Owner getTestcaseOwner(Testcase testcase) {
+    }
+
+    public Hours getHoursInQuarter() {
+    }
+
+    // TODO Este método deve ser removido
+    public void check() {
+        return;
+    }
+
+    /**
+     * Publica resultados com base na data.
+     *
+     * @param date data a partir da qual os resultados devem ser publicados
+     * @return Lista de entidades de resultado
+     */
+    public List<Result> publishResults(Date date) {
+        // alguma lógica de negócios aqui
+        return null;
+    }
+
+    // Classes auxiliares fictícias para compilação
+    static class Owner {}
+    static class Testcase {}
+    static class Hours {}
+    static class Result {}
+}
